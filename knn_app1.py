de4485bb9ec37fa90337715748bb267393344065
Brian McGrail <brianmcgrail@macbookpro.mynetworksettings.com>
2025-08-07 23:05:38 -0400

    Add sklearn version check to Streamlit app

--- knn_app1.py
+++ knn_app1.py
@@ -8,6 +8,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
+st.write("scikit‑learn version (live):", sklearn.__version__)
+
 from sklearn.neighbors import KNeighborsClassifier
 
 # Load historical data
